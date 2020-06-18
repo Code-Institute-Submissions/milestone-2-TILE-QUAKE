ef819964c2f15753e8a11dcf2abe4a6e3b3fe479
devtoguk <devtoguk@gmail.com>
2020-06-18 12:53:19 +0000

    Move the animation timeout into the setup object

--- assets/js/tile-quake.js
+++ assets/js/tile-quake.js
@@ -1,5 +1,10 @@
-const welcomeScreen = document.querySelector('.welcome');
+const setup = {
+  welcomeScreen: () => {
+    const welcomeScreen = document.querySelector('.welcome');
+    setTimeout(() => {
+      welcomeScreen.classList.add('welcome__scale-up');
+    }, 400);
+  }
+}
 
-setTimeout(() => {
-  welcomeScreen.classList.add('welcome__scale-up');
-}, 400);
+setup.welcomeScreen();

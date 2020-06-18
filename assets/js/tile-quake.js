af4460b7d46507173f092ab82165b9ebbf01aca5
devtoguk <devtoguk@gmail.com>
2020-06-18 20:01:53 +0000

    Remove unnecessary grid const and console log messages

--- assets/js/tile-quake.js
+++ assets/js/tile-quake.js
@@ -17,11 +17,11 @@ const setup = {
 
   displayGameSetup: () => {
     const welcomeScreen = document.querySelector('.welcome');
-    welcomeScreen.classList.add('welcome__move-down');
     const setupScreen = document.querySelector('.game-setup');
     const gridSize3x3 = document.querySelector('.grid-size__3x3');
     const gridSize4x4 = document.querySelector('.grid-size__4x4');
     const gridSize5x5 = document.querySelector('.grid-size__5x5');
+    welcomeScreen.classList.add('welcome__move-down');
     gridSize3x3.innerHTML = setup.displayGridSize(3);
     gridSize4x4.innerHTML = setup.displayGridSize(4);
     gridSize5x5.innerHTML = setup.displayGridSize(5);
@@ -30,29 +30,25 @@ const setup = {
 
   eventListeners: () => {
     const newGameButton = document.querySelector('#new--game');
+    const difficultyInput = document.querySelector('#difficulty--input');
+    const gridOptions = document.querySelectorAll('.js-grid-option');
+
     newGameButton.addEventListener('click', setup.displayGameSetup);
     
-    const difficultyInput = document.querySelector('#difficulty--input');
     difficultyInput.addEventListener('change', (e) => {
       const difficultyLevel = document.querySelector('.difficulty__value');
       difficultyLevel.innerHTML = e.target.value;
     });
     
-    const gridOptions = document.querySelectorAll('.js-grid-option');
     gridOptions.forEach(option => {
-      // console.log(option);
       option.addEventListener('click', (e) => {
-        console.log(e);
-        console.log(e.target.parentElement.dataset);
-
         const clickedGridText = e.target.parentElement.dataset['grid'];
-
-        console.log('Text: ', clickedGridText);
         const clickedGrid = e.target.parentElement;
+        const gridSizeValue = document.querySelector('.grid-size__value');
+        console.log('Grid = ', clickedGrid.dataset['grid']);
         gridOptions.forEach(option => { option.classList.remove('grid-size__chosen'); });
         clickedGrid.classList.add('grid-size__chosen');
-        const gridSizeValue = document.querySelector('.grid-size__value');
-        gridSizeValue.innerHTML = clickedGridText;
+        gridSizeValue.innerHTML = clickedGrid.dataset['grid'];
       });
     });
   }

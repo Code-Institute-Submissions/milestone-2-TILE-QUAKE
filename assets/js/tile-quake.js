c61da1c132f4cca9a6071164b812f9a7f7941cd7
devtoguk <devtoguk@gmail.com>
2020-06-18 14:32:19 +0000

    Create an eventlistener for the new game button

--- assets/js/tile-quake.js
+++ assets/js/tile-quake.js
@@ -7,8 +7,14 @@ const setup = {
   },
   
   eventListeners: () => {
+    const newGameButton = document.querySelector('#new--game');
+    const welcomeScreen = document.querySelector('.welcome');
+    newGameButton.addEventListener('click', () => {
+      console.log(welcomeScreen);
+      welcomeScreen.classList.add('welcome__move-down');
+    });
   }
-  
+
 }
 
 setup.welcomeScreen();

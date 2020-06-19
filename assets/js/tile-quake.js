2abc580373f8e8b2d6b01991fc8fae4120b1434e
devtoguk <devtoguk@gmail.com>
2020-06-19 21:02:34 +0000

    Allow selection of a puzzle image in game setup

--- assets/js/tile-quake.js
+++ assets/js/tile-quake.js
@@ -1,4 +1,5 @@
 const setup = {
+  puzzleImageIndex: 1,
   welcomeScreen: () => {
     const welcomeScreen = document.querySelector('.welcome');
     setTimeout(() => {
@@ -15,6 +16,22 @@ const setup = {
     return html;
   },
 
+  selectPuzzleImage: (n) => {
+    const puzzleImage = document.querySelector('.puzzle-image__value');
+    setup.showPuzzleImage(setup.puzzleImageIndex += n);
+    puzzleImage.innerHTML = setup.puzzleImageIndex;
+  },
+
+  showPuzzleImage: (n) => {
+    const puzzleImages = document.getElementsByClassName("puzzle-image__container");
+    if (n > puzzleImages.length) { setup.puzzleImageIndex = 1; }
+    if (n < 1) { setup.puzzleImageIndex = puzzleImages.length; }
+    for (let i = 0; i < puzzleImages.length; i++) {
+        puzzleImages[i].style.display = "none";
+    }
+    puzzleImages[setup.puzzleImageIndex - 1].style.display = "block";
+  },
+
   displayGameSetup: () => {
     const welcomeScreen = document.querySelector('.welcome');
     const setupScreen = document.querySelector('.game-setup');
@@ -25,6 +42,7 @@ const setup = {
     gridSize3x3.innerHTML = setup.displayGridSize(3);
     gridSize4x4.innerHTML = setup.displayGridSize(4);
     gridSize5x5.innerHTML = setup.displayGridSize(5);
+    setup.showPuzzleImage(setup.puzzleImageIndex);
     setupScreen.classList.add('game-setup__move-down');
   },
 

e195b3821020c63cee287dbbc0204b37e8fbe226
devtoguk <devtoguk@gmail.com>
2020-06-30 15:06:39 +0000

    Add 2 more jasmine high enough for scoreboard tests

--- jasmine/scripts/scoreboard.js
+++ jasmine/scripts/scoreboard.js
@@ -4,17 +4,18 @@ const scoreboard = {
   checkExists: () => {
     let newScoreboard = [];
     if (localStorage.getItem("tileQuakeScoreboard") === null) {
-      newScoreboard.push({ user: 'TOP', score: 9000, level: 1 });
-      newScoreboard.push({ user: 'BBB', score: 7000, level: 1 });
-      newScoreboard.push({ user: 'CCC', score: 5000, level: 1 });
-      newScoreboard.push({ user: 'DDD', score: 3000, level: 1 });
-      newScoreboard.push({ user: 'BOT', score: 1000, level: 1 });
+      newScoreboard.push({ user: 'TQA', score: 9000, level: 1 });
+      newScoreboard.push({ user: 'TQA', score: 7000, level: 1 });
+      newScoreboard.push({ user: 'TQA', score: 5000, level: 1 });
+      newScoreboard.push({ user: 'TQA', score: 3000, level: 1 });
+      newScoreboard.push({ user: 'RIC', score: 1000, level: 1 });
       localStorage.setItem('tileQuakeScoreboard', JSON.stringify(newScoreboard));
     }
     return true;
   },
 
   readScores: () => {
+    scoreboard.checkExists();
     let scoreList = localStorage.getItem("tileQuakeScoreboard");
     scoreboard.data = JSON.parse(scoreList);
     return scoreboard.data.length;
@@ -24,7 +25,9 @@ const scoreboard = {
     scoreboard.readScores();
     let scoreboardPosition = scoreboard.data.length + 1;
     scoreboard.data.forEach((scoreEntry, index) => {
-      if (gameScore > scoreEntry.score && scoreboardPosition > scoreboard.data.length) {
+      if ((gameScore > scoreEntry.score && scoreboardPosition > scoreboard.data.length) ||
+          (gameScore === scoreEntry.score && scoreEntry.user === 'TQA')) {
+        console.log(`MyScore = ${gameScore} | User: ${scoreEntry.user}  Score: ${scoreEntry.score}  Index: ${index}`);
         scoreboardPosition = index;
       }
     });
--- jasmine/spec/scoreboardSpec.js
+++ jasmine/spec/scoreboardSpec.js
@@ -17,4 +17,22 @@ describe("Scoreboard", function() {
         });
     });
 
-});
+    describe("Have I made it onto the scoreboard with a score of 6000", function() {
+        it("should return scoreboard position of 2", function() {
+            expect(scoreboard.isAHighScore(6000)).toBe(2);
+        });
+    });
+
+    describe("Have I made it onto the scoreboard with a score of 3000", function() {
+        it("should return scoreboard position of 3", function() {
+            expect(scoreboard.isAHighScore(3000)).toBe(3);
+        });
+    });
+
+    describe("I should NOT get onto the scoreboard with a score of 1000", function() {
+        it("should return scoreboard position of 6", function() {
+            expect(scoreboard.isAHighScore(1000)).toBe(6);
+        });
+    });
+
+  });

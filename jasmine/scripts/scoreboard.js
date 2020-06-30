a87e8770acc0a52d6a467970cdd521840c461d02
devtoguk <devtoguk@gmail.com>
2020-06-30 12:40:16 +0000

    Pass jasmine test for scoreboard read

--- jasmine/scripts/scoreboard.js
+++ jasmine/scripts/scoreboard.js
@@ -1,4 +1,6 @@
 const scoreboard = {
+  data: [],
+
   checkExists: () => {
     let myScoreboard = [];
     if (localStorage.getItem("scoreboard") === null) {
@@ -6,13 +8,15 @@ const scoreboard = {
         myScoreboard.push({ user: 'AAA', score: 1000, level: 1 });
       }
       localStorage.setItem('scoreboard', JSON.stringify(myScoreboard));
-    } else {
-      return true;
     }
+    return true;
   },
 
   readScores: () => {
-    return 5;
+    let scoreList = localStorage.getItem("scoreboard");
+    data = JSON.parse(scoreList);
+    console.log(data);
+    return data.length;
   }
 
 }
